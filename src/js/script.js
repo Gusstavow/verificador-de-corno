5373ea100ff9fcd44d31aa430bca70d72731fb1f
Gustavo Ferreira <gustavobarreto2000@hotmail.com>
2023-07-21 00:45:28 -0300

    estilos e scripts atualizados

--- src/js/script.js
+++ src/js/script.js
@@ -8,66 +8,78 @@ var resposta = document.querySelector("p#resposta");
 
 
 
-CornoInsta();
+
 function Verificar() {
     total = 0;
 
-    for(var i =0; i < escolha_usuario.length; i++){
+    for (var i = 0; i < escolha_usuario.length; i++) {
         if (escolha_usuario[i].checked) {
             total += 10;
-            if(escolha_usuario[0].checked){
-                total += 100;
-
-
-            }
+            
             VerificarTotalPontos();
             // alert("Corno");
-            
-    
+
+
         }
-            
-        
+
+
 
     }
     NenhamaOpcaoSelecionada();
-    
+
 
 }
 
-function VerificarTotalPontos(){
-    
-    if(total >= min_pontos_para_corno){
-        AlertaDeCornoNaTela();
-        
+function VerificarTotalPontos() {
 
-    }else if(total <=10){
+
+    AlertaDeCornoNaTela();
+
+
+
+}
+
+function AlertaDeCornoNaTela() {
+
+
+    if (total <= 10) {
         alert("Cuidado!! grande potencial para se tornar um corno bem manso.");
 
     }
-}
+    else if (total >= 40) {
+        imagem.setAttribute("src", "./src/img/imgAnimal/c-n3.jpg")
+        res.appendChild(imagem)
+        alert("Detectamos um corno manso da pior especie!!!!! Favor retirar-se do meu site!");
+        resposta.innerHTML = "<strong>Corno Manso</strong> detectado com sucesso! Recomendo rever todos os seus conceitos e tentar novamente mais tarde. Obrigado!"
+    }
 
-function AlertaDeCornoNaTela(){
-    alert("Detectamos um corno manso da pior especie!!!!! Favor retirar-se do meu site!");
-    resposta.innerHTML = "<strong>Corno Manso</strong> detectado com sucesso! Recomendo rever todos os seus conceitos e tentar novamente mais tarde. Obrigado!"
-    imagem.setAttribute("src", "./src/img/coringa.jpg")
-    res.appendChild(imagem)
+    else if (total >= 30 && total < 40) {
+        imagem.setAttribute("src", "./src/img/imgAnimal/c-n1.jpg")
+        res.appendChild(imagem)
+
+    } else if (total >= 20 && total < 30) {
+        imagem.setAttribute("src", "./src/img/coringa.jpg")
+        res.appendChild(imagem)
+    }
     res.style.textAlign = "center"
 }
 
-function CornoInsta(){
-    
-    for(var i = 0; i< insta_corno.length;i++){
-      
-            insta_corno[i].addEventListener("click", AlertaDeCornoNaTela);
+
+
+function CornoInsta() {
+
+    for (var i = 0; i < insta_corno.length; i++) {
+
+        insta_corno[i].addEventListener("click", AlertaDeCornoNaTela);
 
     }
 }
 
-function NenhamaOpcaoSelecionada(){
-    if(total == 0){
+function NenhamaOpcaoSelecionada() {
+    if (total == 0) {
         alert("Voce nao escolheu nenhuma das alternativa.Isso mostra que voce e um corno manso que nao acredita em si mesmo.")
-    
+
     }
 }
 
-   
+
