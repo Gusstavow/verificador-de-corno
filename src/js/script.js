674b9f91afb7084dec74b899152fa740ce7887fd
Gustavo Ferreira <gustavobarreto2000@hotmail.com>
2023-07-23 21:16:49 -0300

    modificacoes
    opcao 5 adicionada com pegadinha na checkbox, titulo atualizado,
    transsicoes adicionada e script consertado .

--- src/js/script.js
+++ src/js/script.js
@@ -8,68 +8,103 @@ var resposta = document.querySelector("p#resposta");
 let totalPontos = document.querySelector("#total-pontos");
 
 
+
 function Verificar() {
-    total = 0;
+    total =0;
 
     for (var i = 0; i < escolha_usuario.length; i++) {
         if (escolha_usuario[i].checked) {
             total += 10;
             
-            VerificarTotalPontos();
-            MostrarPontosNaTela();
+           
             // alert("Corno");
+            
 
         }
 
     }
-    NenhamaOpcaoSelecionada();
+    if(total == 0){
+        NenhamaOpcaoSelecionada();
+        return
+
+    }
+    VerificarTotalPontos();
+    MostrarPontosNaTela();
+    
+    
+   
 
 
 }
 
 function MostrarPontosNaTela(){
     totalPontos.innerHTML = `${total} pontos `
+    
 }
 
 function VerificarTotalPontos() {
+    
 
     AlertaDeCornoNaTela();
 
 }
 
 function AlertaDeCornoNaTela() {
+    
 
 
     if (total <= 10) {
         alert("Cuidado!! grande potencial para se tornar um corno bem manso.");
 
     }
-    else if (total >= 40) {
-        imagem.setAttribute("src", "./src/img/imgAnimal/c-n3.jpg")
+    else if (total > 10 && total <= 20) {
+        imagem.setAttribute("src","./src/img/imgAnimal/c-n1.jpg")
         res.appendChild(imagem)
-        alert("Detectamos um corno manso da pior especie!!!!! Favor retirar-se do meu site!");
-        resposta.innerHTML = "<strong>Corno Manso</strong> detectado com sucesso! Recomendo rever todos os seus conceitos e tentar novamente mais tarde. Obrigado!"
+
+        resposta.innerHTML = "<p>Detectamos um corno nivel 1!</p> Mas nao se preocupe ,voce ainda tem cura!"
+        
     }
 
     else if (total >= 30 && total < 40) {
         imagem.setAttribute("src","./src/img/coringa.jpg")
         res.appendChild(imagem)
 
-    } else if (total >= 20 && total < 30) {
-        imagem.setAttribute("src","./src/img/imgAnimal/c-n1.jpg")
+        resposta.innerHTML = "<p>Detectamos um corno nivel 2!</p> Cuidado ,talvez nao tenha mais cura!"
+
+    } else if (total >= 40 && total <=50) {
+       
+
+
+        imagem.setAttribute("src", "./src/img/imgAnimal/c-n3.jpg")
         res.appendChild(imagem)
+
+        alert("Detectamos um corno manso da pior especie!!!!! Favor retirar-se do meu site!");
+        resposta.innerHTML = "<strong>Corno Manso</strong> detectado com sucesso! Recomendo rever todos os seus conceitos e tentar novamente mais tarde. Obrigado!"
     }
+
+
+
     res.style.textAlign = "center"
+    
 }
 
 
 function NenhamaOpcaoSelecionada() {
-    if (total == 0) {
+    
         alert("Voce nao escolheu nenhuma das alternativa.Isso mostra que voce e um corno manso que nao acredita em si mesmo.")
         imagem.setAttribute("src","./src/img/imgAnimal/c-n4.jpg")
         res.appendChild(imagem)
 
-    }
+    
+}
+
+
+
+function trocarOpcao5(){
+
+    let op5 = document.querySelector("label#op-5");
+    op5.innerHTML = "Sou corno!."
+
 }
 
 
